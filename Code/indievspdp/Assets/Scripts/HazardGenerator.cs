0c5ecacfb30084d85de2dbcbcfacbeb7a670e93b
Pauli Vuollet <liquidsilk2@gmail.com>
2014-11-21 18:53:23 +0200

    Added three hazards, fire, hole and tree. Added Hazard Generator and Scroller. Added the bunny sprite.

---
+++ Code/indievspdp/Assets/Scripts/HazardGenerator.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HazardGenerator : MonoBehaviour
+{
+	public GameObject[] hazards;	//the hazards
+
+	float nextHazard;				//time left before creating next hazard <---scrollunits
+	Transform hazardPoint;			//where to create hazards
+
+	void Start ()
+	{
+		nextHazard = 2.0f;
+		hazardPoint = transform;
+	}
+
+	GameObject GetRandomHazardFromList(ref GameObject[] hazardList_)
+	{
+		if (hazardList_ != null && hazardList_.Length != 0)
+		{
+			int i = Random.Range(0, hazardList_.Length);
+
+			if (hazardList_[i] != null)
+				return hazardList_[i];
+		}
+		return null;
+	}
+
+	void CreateRandomHazard()
+	{
+		GameObject hazardObj = GetRandomHazardFromList(ref hazards); 
+		Instantiate(hazardObj, hazardPoint.position, Quaternion.identity);
+	}
+
+	void Update ()
+	{
+		nextHazard -= Time.deltaTime;
+		if (nextHazard <= 0.0f)
+		{
+			CreateRandomHazard();
+			nextHazard = 2.0f;
+		}
+	}
+}
