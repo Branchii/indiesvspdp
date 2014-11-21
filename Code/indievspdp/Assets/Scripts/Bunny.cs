ce8b0011b70ffbb4a452f88ede89834ee59a232d
Ville Ronkainen <finvpower@gmail.com>
2014-11-21 19:22:06 +0200

    Bunny

---
+++ Code/indievspdp/Assets/Scripts/Bunny.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bunny : MonoBehaviour
+{
+    enum BunnyType
+    {
+        None,
+        Constructor,
+        Fireman,
+        Lumberjack
+    }
+
+    BunnyType bunnyType;
+    Animator anim;
+    void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    void Start()
+    {
+        bunnyType = BunnyType.None;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            ChangeType(BunnyType.Fireman);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.transform.name == "Hazard Fire")
+        {
+            if (bunnyType == BunnyType.Fireman)
+            {
+                Debug.Log("Bunnyfiremaaan");
+                col.gameObject.GetComponent<Fire>().Extinguish();
+            }
+            else
+            {
+                Debug.Log("Bunny burns");
+                Destroy(gameObject);
+                col.gameObject.GetComponent<Fire>().Extinguish();
+            }
+        }
+    }
+
+    void ChangeType(BunnyType type)
+    {
+        bunnyType = type;
+
+        switch (bunnyType)
+        {
+            case BunnyType.Fireman:
+                anim.SetBool("fireman", true);
+                break;
+        }
+    }
+}
---
+++ Code/indievspdp/Assets/Scripts/Fire.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fire : MonoBehaviour 
+{
+	void Update () 
+    {
+        transform.position += new Vector3(-2.0f * Time.deltaTime, 0, 0);
+	}
+
+    public void Extinguish()
+    {
+        Debug.Log("Fire was put out");
+        Destroy(gameObject);
+    }
+}
