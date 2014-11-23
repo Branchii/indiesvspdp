6f858c7c3807d42a579c1e02ebb17a05865dd24b
Ville Ronkainen <finvpower@gmail.com>
2014-11-23 15:11:38 +0200

    Looping background.

---
+++ Code/indievspdp/Assets/Scripts/BackgroundTeleporter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundTeleporter : MonoBehaviour
+{
+    float elementWidth;
+    public GameObject otherElement;
+
+    void Awake()
+    {
+        elementWidth = gameObject.GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void FixedUpdate()
+    {
+        if (gameObject.transform.position.x < (Global.sCont.scrollingObjectDeletePointX - (elementWidth / 2)))
+        {
+            gameObject.transform.position = new Vector2(otherElement.transform.position.x + elementWidth, otherElement.transform.position.y);
+        }
+    }
+}
