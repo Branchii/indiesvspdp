522a07490af56a97b825bd7fe657ecc6da85fca8
Pauli Vuollet <liquidsilk2@gmail.com>
2014-11-23 14:53:51 +0200

    Hazards generate more fairly when player is missing specific bunny items. Bubbles generate at randomized positions.

--- Code/indievspdp/Assets/Scripts/BubbleGenerator.cs
+++ Code/indievspdp/Assets/Scripts/BubbleGenerator.cs
@@ -3,12 +3,13 @@ using System.Collections;
 
 public class BubbleGenerator : MonoBehaviour
 {
-	public float rate;				//generation rate
-	public GameObject[] bubbles;	//the bubbles
-	public GameObject bunnyUp;		//bunny up object
+	public float rate;					//generation rate
+	public GameObject[] bubbles;		//the bubbles
+	public GameObject bunnyUp;			//bunny up object
+	public float randomRadius;			//randomize spawn points
 
-	float nextbubble;				//time left before creating next bubbles
-	Transform bubblePoint;			//where to create bubbles
+	float nextbubble;					//time left before creating next bubbles
+	Transform bubblePoint;				//where to create bubbles
 
 	void Awake()
 	{
@@ -19,6 +20,15 @@ public class BubbleGenerator : MonoBehaviour
 	{
 		nextbubble = 0.0f;
 		bubblePoint = transform;
+
+	}
+
+	void CreateAllBubbles()
+	{
+		for (int i = 0; i < bubbles.Length; i++)
+		{
+			CreateBubble(bubbles[i]);
+		}
 	}
 
 	GameObject GetRandomBubbleFromList(ref GameObject[] bubbleList_)
@@ -33,13 +43,30 @@ public class BubbleGenerator : MonoBehaviour
 		return null;
 	}
 
-	void CreateRandomBubble()
+	GameObject CreateBubble(GameObject obj_)
 	{
-		GameObject hazardObj = GetRandomBubbleFromList(ref bubbles);
-		GameObject thisBubble = Instantiate(hazardObj, bubblePoint.position, Quaternion.identity) as GameObject;
+		Vector3 randomPos = new Vector3((Random.value*2.0f-1.0f) * randomRadius,(Random.value*2.0f-1.0f) * randomRadius, 0.0f);
+
+
+
+		//GameObject hazardObj = GetRandomBubbleFromList(ref bubbles);
+		GameObject thisBubble = Instantiate(obj_, bubblePoint.position + randomPos, Quaternion.identity) as GameObject;
 		thisBubble.GetComponent<DragAndDropHandle>().swipeForce = Global.prefCont.bubbleSwipeForce;
 		thisBubble.GetComponent<Rigidbody2D>().AddForce(new Vector2(-50.0f, 0.0f));
 		thisBubble.GetComponent<Scroller>().scrollingSpeedOffset = 0.15f;
+		return thisBubble;
+	}
+
+	void CreateRandomBubble()
+	{
+		GameObject hazardObj = GetRandomBubbleFromList(ref bubbles);
+		CreateBubble(hazardObj);
+
+
+		/*GameObject thisBubble = Instantiate(hazardObj, bubblePoint.position, Quaternion.identity) as GameObject;
+		thisBubble.GetComponent<DragAndDropHandle>().swipeForce = Global.prefCont.bubbleSwipeForce;
+		thisBubble.GetComponent<Rigidbody2D>().AddForce(new Vector2(-50.0f, 0.0f));
+		thisBubble.GetComponent<Scroller>().scrollingSpeedOffset = 0.15f;*/
 
 		//bunnyup?
 		float value = Random.value * 100.0f;
@@ -66,10 +93,11 @@ public class BubbleGenerator : MonoBehaviour
 		}
 		if (value < thres)
 		{
-			GameObject thisBubble2 = Instantiate(bunnyUp, bubblePoint.position, Quaternion.identity) as GameObject;
+			CreateBubble(bunnyUp);
+			/*GameObject thisBubble2 = Instantiate(bunnyUp, bubblePoint.position, Quaternion.identity) as GameObject;
 			thisBubble.GetComponent<DragAndDropHandle>().swipeForce = Global.prefCont.bubbleSwipeForce;
 			thisBubble.GetComponent<Rigidbody2D>().AddForce(new Vector2(-50.0f, 0.0f));
-			thisBubble.GetComponent<Scroller>().scrollingSpeedOffset = 0.15f;
+			thisBubble.GetComponent<Scroller>().scrollingSpeedOffset = 0.15f;*/
 		}
 		
 	}
--- Code/indievspdp/Assets/Scripts/HazardGenerator.cs
+++ Code/indievspdp/Assets/Scripts/HazardGenerator.cs
@@ -5,7 +5,7 @@ public class HazardGenerator : MonoBehaviour
 {
 	public float rate;				//generation rate
 	public float startingTime;		//time before the generator starts (for letting player to gather few bubbles)
-	public GameObject[] hazards;	//the hazards
+	public GameObject[] hazards;	//the hazards, 0 = lumber, 1 = fireman, 2 = pinwheel
 
 	float nextHazard;				//time left before creating next hazard
 	Transform hazardPoint;			//where to create hazards
@@ -33,9 +33,60 @@ public class HazardGenerator : MonoBehaviour
 		return null;
 	}
 
+	GameObject GetRandomHazardFromListFairly(ref GameObject[] hazardList_)
+	{
+		if (hazardList_ != null && hazardList_.Length != 0)
+		{
+			float treeChance = 33.33f;
+			float fireChance = 33.33f;
+			float rainChance = 33.33f;
+
+			float noItemValue = 10.0f;
+
+			if (Global.bunnyList.ReturnItemCount(Bunny.BunnyType.Lumberjack) < 1) //no lumbers
+			{
+				treeChance -= noItemValue;
+			}
+			if (Global.bunnyList.ReturnItemCount(Bunny.BunnyType.Fireman) < 1) //no firemen
+			{
+				fireChance -= noItemValue;
+			}
+			if (Global.bunnyList.ReturnItemCount(Bunny.BunnyType.Pinwheel) < 1) //no pinwheels
+			{
+				rainChance -= noItemValue;
+			}
+
+			float value = Random.value * (treeChance + fireChance + rainChance);
+			int hazType = -1;
+
+			if (value < treeChance)
+			{
+				//Debug.Log("tree, " + value + " < " + treeChance);
+				hazType = (int)Bunny.BunnyType.Lumberjack - 1;
+			}
+			else if (value < (treeChance+fireChance))
+			{
+				hazType = (int)Bunny.BunnyType.Fireman - 1;
+			}
+			else
+			{
+				hazType = (int)Bunny.BunnyType.Pinwheel - 1;
+			}
+
+			//Debug.Log("rand: " + value);
+			//Debug.Log("tc: " + treeChance + " fc: " + fireChance + " rc: " + rainChance);
+			//Debug.Log("hazType: " + hazType);
+
+			return hazardList_[hazType];
+		}
+		return null;
+	}
+
+
+
 	void CreateRandomHazard()
 	{
-		GameObject hazardObj = GetRandomHazardFromList(ref hazards); 
+		GameObject hazardObj = GetRandomHazardFromListFairly(ref hazards); 
 		Instantiate(hazardObj, hazardPoint.position, Quaternion.identity);
 	}
 
